32362f8f08e67ffd8c6ee57d7fd9ee5ee987e3d0
Your0nlySon <eaduncan18@gmail.com>
2021-09-30 15:56:40 -0400

    logic code

--- exercise24/src/main/java/exercise24/base/Solution24.java
+++ exercise24/src/main/java/exercise24/base/Solution24.java
@@ -5,6 +5,8 @@ package exercise24.base;
     Copyright 2021 Ethan Duncan
  */
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Solution24 {
@@ -22,24 +24,52 @@ public class Solution24 {
         Boolean isAnagram = sol.seeIfItIsAnagram(firstString, secondString);
 
         //if isAnagram is true then print 'firstSting and secondString are anagrams'
+        if (isAnagram = true) {
+            System.out.println(firstString + " and " + secondString + " are anagrams");
+        }
         //else print 'firstString and secondString are not anagrams'
+        else {
+            System.out.println(firstString + " and " + secondString + " are not anagrams");
+        }
     }
 
     public Boolean seeIfItIsAnagram(String fS, String sS) {
         //create two new strings
-        //set boolean to true
+        String s1 = fS.replaceAll("//s", "");
+        String s2 = sS.replaceAll("//s", "");
         // create an if else based on if the strings are similar in length
-        //if they aren't similar return false
-        //else change strings into arrays
-        //sort both of the arrays then check if they are equal to one another and if they are return true
+        if (s1.length() != s2.length()) {
+            //if they aren't similar return false
+            return false;
+        }
+        else {
+            //else change strings into arrays
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            //sort both of the arrays then check if they are equal to one another and if they are return true
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            if (Arrays.equals(ArrayS1, ArrayS2)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 
     private String readSecondStringFromUser() {
         //Ask for user input and return the user input
+        System.out.println("Enter the second string: ");
+        String userInput = in.nextLine();
+        return userInput;
     }
 
     private String readFirstStringFromUser() {
         //ask for user input and return the user input
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:\nEnter the first string: ");
+        String userInput = in.nextLine();
+        return userInput;
     }
 
 }
