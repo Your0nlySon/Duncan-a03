08c5e2376eb520ba92d6f44937746db157dca0b5
Your0nlySon <eaduncan18@gmail.com>
2021-09-30 18:22:53 -0400

    logic code

--- exercise25/src/main/java/exercise25/base/Solution25.java
+++ exercise25/src/main/java/exercise25/base/Solution25.java
@@ -23,22 +23,53 @@ public class Solution25 {
 
     private String printTheStrengthOfPassword(int passwordValidator, String password) {
         //Switch statement
-        //Case 0 = The password 'password' is of unknown strength.
-        //Case 1 = The password 'password' is a very weak password.
-        //Case 2 = The password 'password' is a weak password.
-        //Case 3 = The password 'password' is a strong password.
-        //Case 4 = The password 'password' is a very strong password.
+        switch (passwordValidator) {
+            //Case 0 = The password 'password' is of unknown strength.
+            case 0: System.out.println("The password " + password + " is of unknown strength");
+            break;
+            //Case 1 = The password 'password' is a very weak password.
+            case 1: System.out.println("The password " + password + " is very weak password");
+            break;
+            //Case 2 = The password 'password' is a weak password.
+            case 2: System.out.println("The password " + password + " is a weak password");
+            break;
+            //Case 3 = The password 'password' is a strong password.
+            case 3: System.out.println("The password " + password + " is a strong password");
+            break;
+            //Case 4 = The password 'password' is a very strong password.
+            case 4: System.out.println("The password " + password + " is a very strong password");
+            break;
+        }
+        return null;
     }
 
     private String readUserPasswordFromUser() {
         //ask the user for input and return that input
+        System.out.println("Enter Password: ");
+        String pass = in.nextLine();
+        return pass;
     }
 
     public int seeIfPasswordIsStrong(String uP) {
         //if password is all numbers and less than 8 return 1
+        if (uP.matches("[0-9]+") && (uP.length() <= 8)) {
+            return 1;
+        }
         //else if password is all letter and is fever than 8 return 2
+        else if (uP.matches("[a-zA-Z]+") && (uP.length() <= 8)) {
+            return 2;
+        }
         //else if password contains letters and at least one number and is at least 8 return 3
+        else if (uP.matches("(([a-zA-Z].*[0-9])|([0-9].*[a-zA-Z]))") && (uP.length() >= 8)) {
+            return 3;
+        }
         //else if password contains letters, numbers, and special characters and is at least 8 return 4
+        else if (uP.matches("[a-zA-Z0-9-+_!@#$%^&*., ?]+") && (uP.length() >= 8)) {
+            return 4;
+        }
         //else return 0
+        else {
+            return 0;
+        }
     }
 }
