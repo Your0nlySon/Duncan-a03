86f518fb49be08227a1f633b606570c3a02672ab
Your0nlySon <eaduncan18@gmail.com>
2021-10-01 14:15:48 -0400

    Pseudocode

---
+++ exercise30/src/main/java/exercise30/base/Solution30.java
@@ -0,0 +1,29 @@
+package exercise30.base;
+
+/*
+    UCF COP3330 Fall 2021 Assignment 3 Solutions
+    Copyright 2021 Ethan Duncan
+ */
+
+import java.util.Scanner;
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+        Solution30 sol = new Solution30();
+        //initialize the table size
+        //call method 'printMultiplicationTable'
+        sol.printMultiplicationTable(tableSize);
+    }
+
+    private void printMultiplicationTable(int tableSize) {
+        // first print the top header row
+        //use a for loop to write the top numbers 1-12
+
+        //for loop for creating the multiplication and left column values
+        // print left most column first
+        //for loop to calc all the multiplication values
+        }
+    }
+}
+
