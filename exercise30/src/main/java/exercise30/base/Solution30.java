e29feaa111e5cf76b9426fa5488e33762ad7d733
Your0nlySon <eaduncan18@gmail.com>
2021-10-01 20:23:55 -0400

    Logic Code

--- exercise30/src/main/java/exercise30/base/Solution30.java
+++ exercise30/src/main/java/exercise30/base/Solution30.java
@@ -11,18 +11,30 @@ public class Solution30 {
 
     public static void main(String[] args) {
         Solution30 sol = new Solution30();
+
         //initialize the table size
+        int tableSize = 12;
         //call method 'printMultiplicationTable'
         sol.printMultiplicationTable(tableSize);
     }
 
     private void printMultiplicationTable(int tableSize) {
         // first print the top header row
+        System.out.format("      ");
         //use a for loop to write the top numbers 1-12
+        for(int i = 1; i<=tableSize;i++ ) {
+            System.out.format("%4d",i);
+        }
+        System.out.println();
 
-        //for loop for creating the multiplication and left column values
-        // print left most column first
-        //for loop to calc all the multiplication values
+        for(int i = 1 ;i<=tableSize;i++) {
+            // print left most column first
+            System.out.format("%4d  ",i);
+            //for loop to calc all the multiplication values
+            for(int j=1;j<=tableSize;j++) {
+                System.out.format("%4d",i*j);
+            }
+            System.out.println();
         }
     }
 }
