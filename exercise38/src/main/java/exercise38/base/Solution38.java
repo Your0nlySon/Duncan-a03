d3e86df553b8b8be9890541fd8950f96df238a50
Your0nlySon <eaduncan18@gmail.com>
2021-10-03 17:39:28 -0400

    Logic code

--- exercise38/src/main/java/exercise38/base/Solution38.java
+++ exercise38/src/main/java/exercise38/base/Solution38.java
@@ -22,12 +22,29 @@ public class Solution38 {
 
     private String readAskNumFromUser() {
         //ask for user input and return the string
+        System.out.println("Enter a list of numbers, separated by spaces: ");
+        return in.nextLine();
     }
 
     private void filterEvenNumbers(String askNum) {
         //create an array called 'evenArray'
+        char[] evenArray = new char[askNum.length()];
+        int finalArray[];
         //for loop with an if statement that adds the string to an array
+        for (int i = 0; i < askNum.length(); i++) {
+                evenArray[i] = askNum.charAt(i);
+        }
+
         //then use % to find if there is no remainder
+        System.out.print("The even numbers are");
+        for (int i = 0; i < evenArray.length; i++) {
+            if (evenArray[i] % 2 == 0) {
+                System.out.print(evenArray[i]);
+            }
+            else {
+                continue;
+            }
+        }
         //add that value to another array then print the array
     }
 }
