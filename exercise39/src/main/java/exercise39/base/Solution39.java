1044fe3d3350f750df886ed3037fdf34acd5ca75
Your0nlySon <eaduncan18@gmail.com>
2021-10-03 21:43:10 -0400

    Pseudocode

---
+++ exercise39/src/main/java/exercise39/base/Solution39.java
@@ -0,0 +1,23 @@
+package exercise39.base;
+
+/*
+    UCF COP3330 Fall 2021 Assignment 3 Solutions
+    Copyright 2021 Ethan Duncan
+ */
+
+import java.util.Map;
+
+public class Solution39 {
+
+    public static void main(String[] args) {
+        Solution39 sol = new Solution39();
+        //print the top part of the map
+        //call method 'printMap'
+        sol.printMap();
+    }
+
+    private void printMap() {
+        //create the map that can contain three strings
+        //print the map with a for loop
+    }
+}
