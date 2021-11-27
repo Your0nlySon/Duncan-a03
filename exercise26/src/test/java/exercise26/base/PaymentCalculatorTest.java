78993af7dfd127b5b7c0360aff38ab6d56a045c3
Your0nlySon <eaduncan18@gmail.com>
2021-11-27 11:11:44 -0500

    Fixed Test for Assignment

--- exercise26/src/test/java/exercise26/base/PaymentCalculatorTest.java
+++ exercise26/src/test/java/exercise26/base/PaymentCalculatorTest.java
@@ -9,7 +9,16 @@ class PaymentCalculatorTest {
 
     @Test
     public void test_calculateMonthsUntilPaidOff () {
+        PaymentCalculator pC = new PaymentCalculator();
 
+        double b = 5000;
+        double a = 0.12;
+        double p = 100;
+
+        int expected = 70;
+        int actual = pC.PaymentCalculator(b, a, p);
+
+        assertEquals(expected, actual);
     }
 
 }
